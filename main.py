e39fb7e78f3614671087d31a9bac0b1a3e53421d
zntb <zenetibi@yahoo.com>
2025-07-26 07:11:14 +0300

    Storing the Extracted Data in Text Files

--- main.py
+++ main.py
@@ -15,9 +15,26 @@ def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value =extractor.extract(source)["tours"]
     return value
+
+def send_email():
+    print("Email sent")
+    
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+        
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
     
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+   
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
