f760ac1336b14f9bd54710bc45bf5dd01892e432
zntb <zenetibi@yahoo.com>
2025-07-27 09:13:35 +0300

    Storing the App Data in an SQL Database

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 import selectorlib
-import time 
+import time
+import sqlite3
 from send_email import send_email
 
+
+connection = sqlite3.connect('data.db')
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -21,12 +24,23 @@ def extract(source):
     
     
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
-        
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
+    
+    
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
+
     
 
 if __name__ == "__main__":
@@ -34,10 +48,10 @@ if __name__ == "__main__":
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
-    
-        content = read(extracted)
+          
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message=f"Hey, new tour is available! {extracted}")
                 print("Email sent")
